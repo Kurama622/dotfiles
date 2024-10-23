33c441b0423d2acfad10c4f1c8b32281fd54df33
Kurama <kurama174@163.com>
2024-10-23 21:47:59 +0800

    link .zshrc

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-paths=("zsh" ".zshrc" "starship.toml" "tmux" "tmuxinator" "alacritty" "nvim")
+paths=("zsh" "starship.toml" "tmux" "tmuxinator" "alacritty" "nvim")
 for p in "${paths[@]}"; do
   ln -s "$HOME/Github/dotfiles/$p" "$HOME/.config/"
 done
+
+ln -s "$HOME/Github/dotfiles/.zshrc" "$HOME/.zshrc"
