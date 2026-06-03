442c96d185ec20f465f5fde18713bde12d753f6b
Kurama622 <kurama174@163.com>
2026-06-03 22:59:49 +0800

    Use basedir for symbolic links

--- init.sh
+++ init.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+basedir=$(dirname $0)
 paths=("zsh" "starship.toml" "tmux" "nvim" "tmuxp" "kitty" "lazygit" "rofi")
 for p in "${paths[@]}"; do
-  ln -s "$PWD/$p" "$HOME/.config/"
+  ln -s "$basedir/$p" "$HOME/.config/"
 done
 
-ln -s "$PWD/.zshrc" "$HOME/.zshrc"
-ln -s "$PWD/git/.gitconfig" "$HOME/.gitconfig"
+ln -s "$basedir/.zshrc" "$HOME/.zshrc"
+ln -s "$basedir/git/.gitconfig" "$HOME/.gitconfig"
